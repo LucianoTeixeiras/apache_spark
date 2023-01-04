d433111091660847f8427507d432cbfecf1ddfe6
Luciano Teixeira <luciano.teixeiras@gmail.com>
2023-01-04 09:58:41 +0000

    Upd gitignore and setup file

--- Setup/04-spark.sh
+++ Setup/04-spark.sh
@@ -1,13 +1,14 @@
 # Download Apache Spark
-sudo wget https://archive.apache.org/dist/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz
+wget https://archive.apache.org/dist/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz
 
-sudo tar -xzf spark-3.3.1-bin-hadoop3.tgz
+tar -xzf spark-3.3.1-bin-hadoop3.tgz
 
 sudo mv spark-3.3.1-bin-hadoop3 /opt/spark
 
 # Download Apache Hadoop
-sudo wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz
+wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz
+wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4-src.tar.gz
 
-sudo tar -xzf hadoop-3.3.4.tar.gz
+tar -xzf hadoop-3.3.4.tar.gz
 
 sudo mv hadoop-3.3.4 /opt/hadoop
